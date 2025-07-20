951826f0026587603e19701891dec6273d63e018
Habib Hussain <hussainhabib05@yahoo.com>
2025-07-20 20:15:23 +0600

    add auth middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,11 +4,24 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessTokenandRefreshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(500, "Failed to generate tokens");
+  }
+};
+
 const registerUser = asyncHandler(async (req, res) => {
   // get userdetails from request body
   const { fullName, username, email, password } = req.body;
-  console.log("email: ", email);
-  console.log("body: ", req.body);
 
   // validation -not empty
   if (
@@ -26,12 +39,7 @@ const registerUser = asyncHandler(async (req, res) => {
   // check for images, check for avatar
   const avatarLocalPath = req.files?.avatar[0]?.path;
 
-  const converImageLocalPath = req.files?.coverImage[0]?.path;
-
-  if (!avatarLocalPath) {
-    throw new ApiError(400, "Avatar is required");
-  }
-
+  //const converImageLocalPath = req.files?.coverImage[0]?.path;
   let coverImageLocalPath;
   if (
     req.files &&
@@ -39,6 +47,9 @@ const registerUser = asyncHandler(async (req, res) => {
     req.files.coverImage.length > 0
   ) {
     coverImageLocalPath = req.files.coverImage[0].path;
+  }
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar is required");
   }
 
   // upload them to cloudinary, check if avatar is uploaded
@@ -77,4 +88,63 @@ const registerUser = asyncHandler(async (req, res) => {
     .status(201)
     .json(new ApiResponse(201, createdUser, "User registered successfully"));
 });
-export { registerUser };
+
+const loginUser = asyncHandler(async (req, res) => {
+  // return response
+
+  // get user details from request body
+  const { email, username, password } = req.body;
+
+  if (!email && !username) {
+    throw new ApiError(400, "Email or username is required");
+  }
+  // find user
+  const user = await User.findOne({ $or: [{ email }, { username }] });
+
+  if (!user) {
+    throw new ApiError(404, "User doesn't exist");
+  }
+  // check if credientials are correct
+  const isPasswordCValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordCValid) {
+    throw new ApiError(401, "Invalid user credentials");
+  }
+
+  // if correct, generate access token and refresh token
+  const { accessToken, refreshToken } = generateAccessTokenandRefreshToken(
+    user._id
+  );
+
+  loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  // send cookie
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(new ApiResponse(200, loggedInUser, "User logged in successfully"));
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  //remove refresh token from user
+  User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: { refreshToken: undefined },
+    },
+    {
+      new: true,
+    }
+    //remove access token from cookies
+  );
+});
+
+export { registerUser, loginUser, logoutUser };
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+const verfifyJWT = asyncHandler(async (req, res, next) => {
+  try {
+    //grab the token
+    const token =
+      req.cookies.accessToken ||
+      req.headers.authorization?.replace("Bearer ", ""); //.split(" ")[1];
+    //check if token is present
+    if (!token) {
+      throw new ApiError(401, "Access token is required");
+    }
+    //verify and decode the token
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    //use user id from the decoded token and find the user
+    const user = await User.findById(decodedToken._id).select(
+      "-password -refreshToken"
+    );
+
+    //check if user exists
+    if (!user) {
+      throw new ApiError(401, "Invalid access token");
+    }
+
+    //attach user to the request object
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, "Invalid access token");
+  }
+});
